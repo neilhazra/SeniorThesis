Fixed Gradient, Added Gibbs Sampler, Revised Importance Sampling

DiscreteVariationalParameterizations.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn
 import torch.nn as nn
+import math
 
 
 class BoltzmannEncoderDecoder(nn.Module):
@@ -18,67 +19,219 @@ class BoltzmannEncoderDecoder(nn.Module):
     def init(self):
         torch.nn.init.xavier_uniform(self.b)
         torch.nn.init.xavier_uniform(self.c)
-        torch.nn.init.xavier_uniform(self.W)
+        torch.nn.init.xavier_uniform(self.W, gain=1)
 
-    def energy(self, z, y):
-        e1 = -self.b.view(1, 1, self.out_dim) @ z.view(self.batch_size, self.out_dim, 1)
-        e2 = -self.c.view(1, 1, self.in_dim) @ y.view(self.batch_size, self.in_dim, 1)
-        e3 = -self.b.view(1, 1, self.out_dim) @ self.W @ y.view(self.batch_size, self.in_dim, 1)
-        return (e1 + e2 + e3).squeeze(-1)
+    @staticmethod
+    def conditional_log_probability_x_given_w(w, x, W, c):
+        batch_size = x.shape[0]
+        return torch.nn.functional.logsigmoid((2 * x.view(batch_size, -1, 1) - 1) *
+                                              (c.unsqueeze(-1) + torch.transpose(W, 1, 2) @
+                                               w.view(batch_size, -1, 1))).sum(dim=-2)
 
-    def conditional_log_probability_y_given_z(self, z, y):
-        return torch.nn.functional.logsigmoid((2 * y.view(self.batch_size, self.in_dim, 1) - 1) *
-                                              (self.c.unsqueeze(-1) + torch.transpose(self.W, 1, 2) @
-                                               z.view(self.batch_size, self.out_dim, 1))).sum(dim=-2)
+    @staticmethod
+    def conditional_log_probability_w_given_x(w, x, W, b):
+        batch_size = x.shape[0]
+        return torch.nn.functional.logsigmoid((2 * w.view(batch_size, -1, 1) - 1) *
+                                              (b.unsqueeze(-1) + W @ x.view(batch_size, -1,
+                                                                            1))).sum(dim=-2)
 
-    def conditional_log_probability_z_given_y(self, z, y):
-        return torch.nn.functional.logsigmoid((2 * z.view(self.batch_size, self.out_dim, 1) - 1) *
-                                              (self.b.unsqueeze(-1) + self.W @ y.view(self.batch_size, self.in_dim,
-                                                                                      1))).sum(dim=-2)
+    @staticmethod
+    def encoder_sample(x, W, b):
+        batch_size = x.shape[0]
+        thresholds = torch.sigmoid(
+            (b.unsqueeze(-1) + W @ x.view(batch_size, -1, 1)))
+        return (torch.rand_like(thresholds) < thresholds).float().squeeze(-1)
+
+    @staticmethod
+    def decoder_sample(w, W, c):
+        batch_size = w.shape[0]
+        thresholds = torch.sigmoid(
+            (c.unsqueeze(-1) + torch.transpose(W, 1, 2) @ w.view(batch_size, -1, 1)))
+        return (torch.rand_like(thresholds) < thresholds).float().squeeze(-1)
+
+    def conditional_log_probability_x_given_w_(self, w, x):
+        return BoltzmannEncoderDecoder.conditional_log_probability_x_given_w(w, x, self.W, self.c)
+
+    def conditional_log_probability_w_given_x_(self, w, x):
+        return BoltzmannEncoderDecoder.conditional_log_probability_w_given_x(w, x, self.W, self.b)
+
+    def conditional_log_probability_w_given_x_double_batched_(self, w, x):
+        dim_0 = w.shape[0]
+        dim_1 = w.shape[1]
+        return BoltzmannEncoderDecoder.conditional_log_probability_w_given_x(w.reshape(dim_0 * dim_1, -1),
+                                                                             x.reshape(dim_0 * dim_1, -1), self.W,
+                                                                             self.b).reshape(dim_0, dim_1, -1)
+
+    def conditional_log_probability_w_given_x_double_batched(w, x, W, b):
+        dim_0 = w.shape[0]
+        dim_1 = w.shape[1]
+        return BoltzmannEncoderDecoder.conditional_log_probability_w_given_x(w.reshape(dim_0 * dim_1, -1),
+                                                                             x.reshape(dim_0 * dim_1, -1), W,
+                                                                             b).reshape(dim_0, dim_1, -1)
 
     # simple since factorial distribution
-    def encoder_sample(self, y):
-        thresholds = torch.sigmoid(
-            (self.b.unsqueeze(-1) + self.W @ y.view(self.batch_size, self.in_dim, 1)))
-        return (torch.rand_like(thresholds) < thresholds).float().squeeze(-1)
+    def encoder_sample_(self, x):
+        return BoltzmannEncoderDecoder.encoder_sample(x, self.W, self.b)
 
-    def decoder_sample(self, z):
-        thresholds = torch.sigmoid(
-            (self.c.unsqueeze(-1) + torch.transpose(self.W, 1, 2) @ z.view(self.batch_size, self.out_dim, 1)))
-        return (torch.rand_like(thresholds) < thresholds).float().squeeze(-1)
+    # simple since factorial distribution
+    def batched_encoder_sample_(self, x):
+        dim_0 = x.shape[0]
+        dim_1 = x.shape[1]
+        return BoltzmannEncoderDecoder.encoder_sample(x.reshape(dim_0 * dim_1, -1), self.W, self.b).reshape(dim_0,
+                                                                                                            dim_1, -1)
+
+    # simple since factorial distribution
+    @staticmethod
+    def batched_encoder_sample(x, W, b):
+        dim_0 = x.shape[0]
+        dim_1 = x.shape[1]
+        return BoltzmannEncoderDecoder.encoder_sample(x.reshape(dim_0 * dim_1, -1), W, b).reshape(dim_0, dim_1, -1)
+
+    def decoder_sample_(self, w):
+        return BoltzmannEncoderDecoder.decoder_sample(w, self.W, self.c)
 
 
 class EnergyBasedModelEmbeddingDynamics(nn.Module):
     def __init__(self, dim):
         super().__init__()
         self.dim = dim  # dimension of y
-        self.energy_function = nn.Sequential(
-            nn.Linear(self.dim + self.dim, 2*self.dim),
-            nn.ReLU(),
-            nn.Linear(2*self.dim, 1)
-        )
+        self.linear_1_weight = nn.Parameter(torch.zeros((2 * self.dim, 2 * self.dim)))
+        self.linear_1_bias = nn.Parameter(torch.zeros((2 * self.dim)))
+        self.linear_2_weight = nn.Parameter(torch.zeros((1, 2 * self.dim)))
+        self.linear_2_bias = nn.Parameter(torch.zeros((1)))
+        self.init()
+
+    def init(self):
+        torch.nn.init.xavier_uniform(self.linear_1_weight)
+        torch.nn.init.xavier_uniform(self.linear_2_weight)
+
+    def unnormalized_log_probs_w_given_z_double_batched_(self, z, w):
+        return EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z_double_batched(z, w,
+                                                                                                 self.linear_1_weight,
+                                                                                                 self.linear_1_bias,
+                                                                                                 self.linear_2_weight,
+                                                                                                 self.linear_2_bias)
+
+    @staticmethod
+    def energy_function(i, W1, b1, W2, b2):
+        temp = torch.nn.functional.linear(i, W1, b1)
+        temp = torch.nn.functional.relu(temp)
+        o = torch.nn.functional.linear(temp, W2, b2)
+        return o
+
+    @staticmethod
+    def _energy(z, w, W1, b1, W2, b2):
+        return EnergyBasedModelEmbeddingDynamics.energy_function(torch.concat([z, w], dim=-1), W1, b1, W2, b2)
+
+    @staticmethod
+    def unnormalized_log_probs_w_given_z(z, w, W1, b1, W2, b2):
+        return -EnergyBasedModelEmbeddingDynamics._energy(z, w, W1, b1, W2, b2)
+
+    def unnormalized_log_probs_w_given_z_(self, z, w):
+        return -EnergyBasedModelEmbeddingDynamics._energy(z, w, self.linear_1_weight, self.linear_1_bias,
+                                                          self.linear_2_weight, self.linear_2_bias)
+
+    @staticmethod
+    def unnormalized_log_probs_w_given_z_double_batched(z, w, W1, b1, W2, b2):
+        first_dim = z.shape[0]
+        second_dim = z.shape[1]
+        dim = z.shape[2]
+        energy = EnergyBasedModelEmbeddingDynamics._energy(z.reshape(-1, dim), w.reshape(-1, dim), W1, b1, W2,
+                                                           b2).reshape(first_dim, second_dim, 1)
+        return -energy
+
+    @staticmethod
+    def expected_unnormalized_log_probs_w_given_z(z, w, W1, b1, W2, b2):
+        samples_dim = z.shape[0]
+        batch_dim = z.shape[1]
+        dim = z.shape[2]
+        energy = EnergyBasedModelEmbeddingDynamics._energy(z.reshape(-1, dim), w.reshape(-1, dim), W1, b1, W2,
+                                                           b2).reshape(samples_dim, batch_dim, 1)
+        return -energy.mean(dim=0)
 
     # for small state spaces it is possible to manually compute the partition function
-    def log_partition_function(self, W):
-        W = W.view(-1, 1, self.dim).expand(-1, 2 ** self.dim, -1)
-        Z = torch.arange(0, 2 ** self.dim).unsqueeze(-1).bitwise_and(2 ** torch.arange(
-            self.dim)).ne(0).unsqueeze(0).expand(W.shape[0], -1, -1)
-        log_probs = self.unnormalized_log_probs_z_given_w_double_batched(Z, W)
+    @staticmethod
+    def log_partition_function(z, W1, b1, W2, b2):
+        dim = z.shape[-1]
+        z = z.view(-1, 1, dim).expand(-1, 2 ** dim, -1)
+        W = torch.arange(0, 2 ** dim, device=z.device).unsqueeze(-1).bitwise_and(2 ** torch.arange(
+            dim, device=z.device)).ne(0).unsqueeze(0).expand(z.shape[0], -1, -1)
+        log_probs = EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z_double_batched(z, W, W1, b1, W2,
+                                                                                                      b2)
         partitions = torch.logsumexp(log_probs, dim=1)
         return partitions
 
-    def normalized_log_probabilities_z_given_w(self, z, w):
-        return self.unnormalized_log_probs_z_given_w(z, w) - self.log_partition_function(w)
+    # the initial state will help create the optimal proposal distribution
+    @staticmethod
+    @torch.no_grad()
+    def estimated_log_partition_function_better(z, initial_state, W, b, _, W1, b1, W2, b2, samples=512):
+        z = z.expand(samples, -1, -1)
+        initial_state = initial_state.expand(samples, -1, -1)
+        w_batched = BoltzmannEncoderDecoder.batched_encoder_sample(initial_state, W, b)
+        proposal_log_probs = BoltzmannEncoderDecoder.conditional_log_probability_w_given_x_double_batched(w_batched,
+                                                                                                          initial_state,
+                                                                                                          W, b)
+        log_probs = EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z_double_batched(z, w_batched, W1,
+                                                                                                      b1, W2, b2)
+        return - math.log(samples) + torch.logsumexp(log_probs - proposal_log_probs, dim=0)
 
-    def _energy(self, z, w):
-        return self.energy_function(torch.concat([z, w], dim=-1))
+    # do some importance sampling here, note that this is probably good enough for training where the gradient can be noisy
+    # and is in generally the right direction, but is definitely not good enough for evaluation
+    @staticmethod
+    @torch.no_grad()
+    def estimated_log_partition_function(z, W1, b1, W2, b2, samples=65536):
+        dim = z.shape[-1]
+        z = z.expand(samples, -1, -1)
+        w = (torch.rand_like(z) < 0.5).float()
+        log_probs = EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z_double_batched(z, w, W1, b1, W2,
+                                                                                                      b2)
+        return dim * math.log(2) - math.log(samples) + torch.logsumexp(log_probs, dim=0)
 
-    def unnormalized_log_probs_z_given_w(self, z, w):
-        return -self._energy(z, w)
+    def estimated_log_partition_function_(self, z):
+        return EnergyBasedModelEmbeddingDynamics.estimated_log_partition_function(z, self.linear_1_weight,
+                                                                                  self.linear_1_bias,
+                                                                                  self.linear_2_weight,
+                                                                                  self.linear_2_bias)
 
-    def unnormalized_log_probs_z_given_w_double_batched(self, z, w):
-        batch_dim = z.shape[0]
-        sample_dim = z.shape[1]
-        dim = z.shape[2]
-        energy = self._energy(z.reshape(-1, dim), w.reshape(-1, dim)).reshape(batch_dim, sample_dim, 1)
-        return -energy
+    @staticmethod
+    def normalized_log_probabilities_w_given_z(z, w, W1, b1, W2, b2):
+        return EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z(z, w, W1, b1, W2,
+                                                                                  b2) - EnergyBasedModelEmbeddingDynamics.log_partition_function(
+            z, W1, b1, W2, b2)
+
+    def normalized_log_probabilities_w_given_z_(self, z, w):
+        return EnergyBasedModelEmbeddingDynamics.normalized_log_probabilities_w_given_z(z, w, self.linear_1_weight,
+                                                                                        self.linear_1_bias,
+                                                                                        self.linear_2_weight,
+                                                                                        self.linear_2_bias)
+
+    def estimated_normalized_log_probabilities_w_given_z_(self, z, w):
+        return EnergyBasedModelEmbeddingDynamics.estimated_normalized_log_probabilities_w_given_z(z, w,
+                                                                                                  self.linear_1_weight,
+                                                                                                  self.linear_1_bias,
+                                                                                                  self.linear_2_weight,
+                                                                                                  self.linear_2_bias)
+
+    @staticmethod
+    def estimated_normalized_log_probabilities_w_given_z(z, w, W1, b1, W2, b2):
+        return EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z(z, w, W1, b1, W2,
+                                                                                  b2) - EnergyBasedModelEmbeddingDynamics.estimated_log_partition_function(
+            z, W1, b1, W2, b2)
+
+    @staticmethod
+    def estimated_normalized_log_probabilities_w_given_z_better(z, w, x, W, b, _, W1, b1, W2, b2):
+        return EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z(z, w, W1, b1, W2,
+                                                                                  b2) - EnergyBasedModelEmbeddingDynamics.estimated_log_partition_function_better(
+            z, x, W, b, None, W1, b1, W2, b2)
+
+    @staticmethod
+    def estimated_normalized_log_probabilities_w_given_z_better_(z, w, x, model, samples=512):
+        z_tilde = z.expand(samples, -1, -1)
+        initial_state = x.expand(samples, -1, -1)
+        w_tilde = model.encoder_decoder.batched_encoder_sample_(initial_state)
+        proposal_log_probs = model.encoder_decoder.conditional_log_probability_w_given_x_double_batched_(w_tilde,
+                                                                                                         initial_state)
+        log_probs = model.embedding_dynamics.unnormalized_log_probs_w_given_z_double_batched_(z_tilde, w_tilde)
+        return model.embedding_dynamics.unnormalized_log_probs_w_given_z_(z, w) + math.log(samples) - torch.logsumexp(
+            log_probs - proposal_log_probs, dim=0)
+

SSEPDatasetGenerator.py
@@ -19,16 +19,16 @@ class SEPGenerator:
         # all the data for the dataset (# of bits == samples * space size
         self.data = torch.zeros((self.num_samples, self.space_size), dtype=torch.bool)
 
-        self.random_initializer()
+        self.semi_random_initializer()
+
         #self.initializer()  # use a  simple initializers that initializes all the particles on one side
 
         self.num_particles = torch.sum(self.data, dim=-1, keepdim=True)  # number of particles in each sample
 
         # use fact that minimum of n mean 1 exponentials is a mean n exponential
         # this is the property behind poisson superposition
-        self.exponential = torch.distributions.exponential.Exponential(self.num_particles[0].squeeze().float())
-        self.cumulative_time = torch.zeros_like(self.exponential.sample(sample_shape=torch.Size([self.num_samples])))
-
+        self.exponential = torch.distributions.exponential.Exponential(self.num_particles.float(), validate_args=False)
+        self.cumulative_time = torch.zeros_like(self.exponential.sample())
         # using a bernoulli distribution to generate the jump directions
         self.right_jump = torch.distributions.bernoulli.Bernoulli(probs=right_probability)
 
@@ -37,26 +37,31 @@ class SEPGenerator:
     def initializer(self):
         self.data[:, self.space_size // 2:] = True
 
-    def random_initializer(self):
+    def semi_random_initializer(self):
         x = np.arange(self.space_size)
         rng = np.random.default_rng()
         perms = rng.permuted(np.tile(x, self.num_samples).reshape(self.num_samples, x.size), axis=1)[:, :self.space_size//self.inverse_density]
         self.data[np.arange(self.num_samples).reshape(self.num_samples, 1), perms] = True
 
+    def random_initializer(self):
+        self.data = torch.rand_like(self.data, dtype=torch.float) < 1/self.inverse_density
+
+
     def restart_time(self):
-        self.cumulative_time = torch.zeros_like(self.exponential.sample(sample_shape=torch.Size([self.num_samples])))
+        self.cumulative_time = torch.zeros_like(self.exponential.sample())
 
     def increment_time(self, time):
         self.time_period += time
 
     def step(self):
-        new_wait_times = self.exponential.sample(sample_shape=torch.Size([self.num_samples]))
+        new_wait_times = self.exponential.sample()
         self.cumulative_time += new_wait_times
         # mask out the processes that are done updating
         running_samples_indices = torch.nonzero(self.cumulative_time <= self.time_period).squeeze()
         hot_particles = torch.distributions.categorical.Categorical(probs=self.data.float() / self.num_particles)
-        initial_indices = hot_particles.sample()
 
+        #todo watch out for nans
+        initial_indices = hot_particles.sample()
         right_left_jump = (self.right_jump.sample(sample_shape=initial_indices.shape) * 2 - 1).to(torch.int64)
 
         # we want translational symmetry

TrainDiscreteEmbeddingMI.py
@@ -1,9 +1,10 @@
-import math
 import torch
 import torch.nn as nn
 import DiscreteVariationalParameterizations as DVP
 from SSEPDatasetGenerator import SEPGenerator
-from MetropolisHastings import BatchedConditionalMetropolisSampler
+from GibbsSampling import BatchedConditionalGibbsSampler
+from torch.autograd.functional import vjp
+from torch.autograd.function import Function
 
 
 class EmbeddingMI1(nn.Module):
@@ -11,62 +12,87 @@ class EmbeddingMI1(nn.Module):
         super().__init__()
         self.encoder_decoder = DVP.BoltzmannEncoderDecoder(batch_size=batch_size, in_dim=in_dim, out_dim=out_dim)
         self.embedding_dynamics = DVP.EnergyBasedModelEmbeddingDynamics(dim=out_dim)
-        self.metropolis_sampler = BatchedConditionalMetropolisSampler(batch_size=batch_size, num_samples=50, k=2,
-                                                                      mixing_time=200,
-                                                                      conditional_distribution=self.embedding_dynamics)
+        self.sampler = BatchedConditionalGibbsSampler(batch_size=batch_size, num_samples=256,  # needs to be tuned
+                                                      mixing_time=4,  # seems like this can be low and still work
+                                                      joint_distribution=self.embedding_dynamics)
+        self.loss_func = CustomMutualInformationLoss.apply
 
-    # this is not really the objective but it's gradient is approximately equal to that of the objectives
-    # i.e. this is off by a constant, but is differentiable efficiently
-    def quasi_objective(self, x, y):
-        W = self.encoder_decoder.encoder_sample(x)
-        Z = self.encoder_decoder.encoder_sample(y)
-        l1 = self.encoder_decoder.conditional_log_probability_y_given_z(Z, y) - self.encoder_decoder. \
-            conditional_log_probability_z_given_y(Z, y)
-        l2 = self.embedding_dynamics.unnormalized_log_probs_z_given_w(Z, W)
-        l3 = -self.metropolis_sampler.estimate_conditional_expected_value(W)
-        quasi_objective = l1 + l2 + l3  # quasi since this is off by a constant (we get around the intractable Z)
-        return -quasi_objective.mean()
+    def NegativeMILoss(self, x, y):
+        w = self.encoder_decoder.encoder_sample_(x).detach()
+        z = self.encoder_decoder.encoder_sample_(y).detach()
+        w_tilde = self.sampler.run_batched_gibbs(z).detach()
 
-    def full_objective(self, x, y):
-        W = self.encoder_decoder.encoder_sample(x)
-        Z = self.encoder_decoder.encoder_sample(y)
-        objective = self.encoder_decoder.conditional_log_probability_y_given_z(Z, y) - self.encoder_decoder. \
-            conditional_log_probability_z_given_y(Z,
-                                                  y) + self.embedding_dynamics.normalized_log_probabilities_z_given_w(Z,
-                                                                                                                      W)
-        return -objective.mean()
+        return -self.loss_func(
+            z, y, w, x, w_tilde, self.encoder_decoder.W, self.encoder_decoder.b, self.encoder_decoder.c,
+            self.embedding_dynamics.linear_1_weight, self.embedding_dynamics.linear_1_bias,
+            self.embedding_dynamics.linear_2_weight, self.embedding_dynamics.linear_2_bias)
 
 
-def run_dim_red_process(state_space, inverse_density, embedding_space_size, time_period, num_steps = 8000):
-    model = EmbeddingMI1(512, state_space, embedding_space_size)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+# we got to do this the old fashioned way because the random sampling does not propagate the
+# expected gradient
+class CustomMutualInformationLoss(Function):
+    # Note that forward, setup_context, and backward are @staticmethods
+    def forward(ctx, z, y, w, x, w_tilde, W, b, c, W1, b1, W2, b2):
+        p_x_w = DVP.BoltzmannEncoderDecoder.conditional_log_probability_x_given_w(w, x, W, c)
+        p_w_x = DVP.BoltzmannEncoderDecoder.conditional_log_probability_w_given_x(w, x, W, b)
 
+        # oom if embedding size is too large
+        # r_w_z = DVP.EnergyBasedModelEmbeddingDynamics.normalized_log_probabilities_w_given_z(z, w, W1, b1, W2, b2)
+        temp = DVP.EnergyBasedModelEmbeddingDynamics.estimated_normalized_log_probabilities_w_given_z_better(z, w, x, W,
+                                                                                                             b, c, W1,
+                                                                                                             b1, W2, b2)
+        r_w_z = torch.minimum(temp, torch.zeros_like(
+            temp))  # importance sampling underestimtaes the partition function so we must clamp at 0
+
+        ctx.save_for_backward(z, y, w, w_tilde, x, W, b, c, W1, b1, W2, b2, p_x_w - p_w_x, r_w_z)
+
+        # ctx.save_for_backward(z,y,w, w_tilde, x,W,b,c,W1, b1, W2, b2, p_x_w - p_w_x, torch.tensor([-1.0], device=z.device))
+        # because r is hard to estimate maybe the gradient will roughly be in the right direction this way anyway
+        return p_x_w - p_w_x + r_w_z
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        z, y, w, w_tilde, x, W, b, c, W1, b1, W2, b2, encoder_decoder, embedding = ctx.saved_tensors
+        _, (_, _, temp_W_1, temp_c) = vjp(DVP.BoltzmannEncoderDecoder.conditional_log_probability_x_given_w,
+                                          (w, x, W, c), grad_output, create_graph=False)
+        _, (_, _, temp_W_2, temp_b_1) = vjp(DVP.BoltzmannEncoderDecoder.conditional_log_probability_w_given_x,
+                                            (w, x, W, b), grad_output * (embedding + encoder_decoder - 1),
+                                            create_graph=False)
+        _, (_, _, temp_W_3, temp_b_2) = vjp(DVP.BoltzmannEncoderDecoder.conditional_log_probability_w_given_x,
+                                            (z, y, W, b), grad_output * embedding, create_graph=False)
+        _, (_, _, temp_linear_W_1, temp_linear_b_1, temp_linear_W_2, temp_linear_b_2) = vjp(
+            DVP.EnergyBasedModelEmbeddingDynamics.unnormalized_log_probs_w_given_z, (z, w, W1, b1, W2, b2), grad_output,
+            create_graph=False)
+        _, (_, _, temp_1_linear_W_1, temp_1_linear_b_1, temp_1_linear_W_2, temp_1_linear_b_2) = vjp(
+            DVP.EnergyBasedModelEmbeddingDynamics.expected_unnormalized_log_probs_w_given_z,
+            (z.expand(w_tilde.shape[0], -1, -1), w_tilde, W1, b1, W2, b2), grad_output, create_graph=False)
+        return None, None, None, None, None, temp_W_1 + temp_W_2 + temp_W_3, temp_b_1 + temp_b_2, temp_c, temp_linear_W_1 - temp_1_linear_W_1, temp_linear_b_1 - temp_1_linear_b_1, temp_linear_W_2 - temp_1_linear_W_2, temp_linear_b_2 - temp_1_linear_b_2
+
+
+def run_dim_red_process(state_space, inverse_density, embedding_space_size, time_period, num_steps=2000):
+    model = EmbeddingMI1(256, state_space, embedding_space_size)
+    #model.load_state_dict(torch.load('32_32.model'))
+    model = model.cpu()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
     for i in range(num_steps):
-        gen = SEPGenerator(space_size=state_space, num_samples=512, time_period=time_period,
+        gen = SEPGenerator(space_size=state_space, num_samples=256, time_period=time_period,
                            inverse_density=inverse_density)
-        initial_state = gen.data.clone()
+        initial_state = gen.data.clone().cpu()
         gen.run()
-        final_state = gen.data.clone()
-        # print((initial_state != final_state).sum(dim = -1))
+        final_state = gen.data.clone().cpu()
         optimizer.zero_grad()
-        loss = model.quasi_objective(initial_state.float(), final_state.float())
+        loss = model.NegativeMILoss(initial_state.float(), final_state.float()).mean()
         loss.backward()
-        # this is for debugging purposes and only possible with small state spaces
-        if i % 10 == 0:
-            with torch.no_grad():
-                score = model.full_objective(initial_state.float(), final_state.float())
-            print('Iteration', i, 'I(W,Z) > ',
-                  -score.detach().cpu().item() + math.log(math.comb(state_space, state_space // inverse_density)))
-        else:
-            print('Iteration', i)
+        print('Iteration', i, 'I(W,Z) - H(X) > ',
+              -loss.detach().cpu().item())
         optimizer.step()
+    torch.save(model.state_dict(), '32_32.model')
 
-experiments = [(10, 2, 10, 0), (16, 2, 16, 0.05), (16, 2, 16, 0.1), (16, 2, 16, 0.15),
-                (16, 2, 8, 0), (16, 2, 8, 0.1), (16, 2, 8, 0.15), (16, 2, 8, 0.25),
-               (16, 2, 4, 0), (16, 2, 4, 0.1), (16, 2, 4, 0.15), (16, 2, 4, 0.25)]
 
-results = []
-for i, params in enumerate(experiments):
-    print('Running Experiment', i)
-    results.append(run_dim_red_process(*params))
-print(results)
+if __name__ == '__main__':
+    experiments = [(32, 4, 32, 0.001)]
+    results = []
+    for i, params in enumerate(experiments):
+        print('Running Experiment', i)
+        results.append(run_dim_red_process(*params))
+    print(results)